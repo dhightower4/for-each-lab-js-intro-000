f07185a2df615199a2e1f4142872a34bd28bdbfe
Donte Hightower <dontehightower12@gmail.com>
2018-01-09 04:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function iterativeLog(array) {
     console.log(`${index}: ${element}`)
   })
 }
+
+function iterate(callback) {
+  var array = ['Christmas', 'New Years', 'Chelsie\'s Birthday', 'V-Day', 'April 21']
+  array.forEach(callback)
+}
